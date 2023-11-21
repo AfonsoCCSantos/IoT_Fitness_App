f4bf9e43097a7f3abaa7b8a4d029a4238d5b3117
Afonso Santos <afonso.acc.santos@gmail.com>
2023-11-21 16:06:43 +0000

    fixed bug in clean_given_data.py It didnt count the date

--- cloud-Infrastructure/data/app-data/clean_given_data.py
+++ cloud-Infrastructure/data/app-data/clean_given_data.py
@@ -2,11 +2,12 @@ import csv
 from datetime import datetime, timedelta
 
 def is_after(date_str1, date_str2):
+    format_str = "%d/%m/%y %H:%M:%S:%f"
     date_str1 = date_str1[:-4]
     date_str2 = date_str2[:-4]
-    date1 = datetime.strptime(date_str1, "%H:%M:%S:%f")
-    date2 = datetime.strptime(date_str2, "%H:%M:%S:%f")
-    return date1 > date2
+    date1 = datetime.strptime(date_str1, format_str)
+    date2 = datetime.strptime(date_str2, format_str)
+    return date1 >= date2
 
 with open('data/training.data', 'r') as csvfile:
      with open('data/output.data', 'w', newline='') as output_csvfile:
@@ -16,12 +17,11 @@ with open('data/training.data', 'r') as csvfile:
         for row in csvreader:
             training_lines.append(row)
         
-        last_line = training_lines[0][1]
-
+        last_line = training_lines[0][0] + " " + training_lines[0][1]
+        
         for line in training_lines:
-            last_hours, last_minutes, last_seconds, _ = last_line.split(":")
-            hours, minutes, seconds, _ = line[1].split(":")
-            if is_after(line[1], last_line):
-                last_line = line[1]
+            formatted_line = line[0] + " " + line[1]
+            if is_after(formatted_line, last_line):
+                last_line = formatted_line
                 writer.writerow(line)
         
